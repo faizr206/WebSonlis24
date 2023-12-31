df0b297db0a4e2449710e1d6c64a32a9c3aff460
faizr206 <faizrizki.faizrizki@gmail.com>
2023-12-31 20:21:03 +0700

    more work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,22 +8,16 @@
     <title>Home Page</title>
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ url('/css/home.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/fontawesome.min.css"
-        integrity="sha512-d0olNN35C6VLiulAobxYHZiXJmq+vl+BGIgAxQtD5+kqudro/xNMvv2yIHAciGHpExsIbKX3iLg+0B6d0k4+ZA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="font-[montserrat] bg-[#edf1be] overflow-x-hidden">
     <div class="flex justify-center items-center w-full h-[70vw] bg-cover"
-        style="background-image: url('/img/welcomePage.png')">
+        style="background-image: url({{ url('img/welcomePage.png') }})">
         <img src="{{ url('/img/headline.png') }}" class="w-3/5 -translate-y-1/4" alt="">
     </div>
     <div class="relative">
         <img src="{{ url('/img/batasAtas.png') }}" class="absolute -translate-y-[5vw] w-[200vw]" alt="">
-        <div class="flex flex-col items-center justify-center text-3xl z-10 pt-[5vw]">
+        <div class="flex flex-col items-center justify-center md:text-3xl text-[3vw] z-10 pt-[5vw]">
             <img src="{{ url('img/registration.svg') }}" class="w-[50%] -translate-y-[2vw] pb-[3vw]" alt="">
             <div id="countdown" class="flex justify-center font-montserrat text-[#7fb051]">
                 <ul class="w-[75vw] grid grid-cols-7 items-center justify-center">
@@ -50,21 +44,25 @@
         <div class="pt-[2vw] bg-[#def2fd]">
             <h1 class=" text-center text-[6vw] font-sunday text-[#3b73b9] strokeme">COMPETITION
             </h1>
-            <div class="relative">
+            <div class="relative hidden">
                 <img src="{{ url('img/blade1.svg') }}" class="absolute z-30 w-[10vw] drop-shadow-2xl blade1"
+                    alt="">
+                <img src="{{ url('img/blade1.svg') }}" class="absolute z-30 w-[10vw] drop-shadow-2xl blade2"
                     alt="">
             </div>
             <div
-                class="flex flex-row justify-center items-center gap-x-10 font-montserrat font-bold text-3xl text-[#3b73b9] p-5">
+                class="flex flex-row justify-center items-center gap-x-10 font-montserrat font-bold text-2xl md:text-3xl text-[#3b73b9] p-5">
                 <button href="#">SMP</button>
                 <button href="#">SMA</button>
             </div>
             <div id="lombaSMP">
-                <div class="bg-[#c4dcae] flex flex-col justify-center items-center" onmouseenter="enterSaintek()"
-                    onmouseleave="leaveSaintek()">
-                    <img src="{{ url('/img/jenis/saintek.png') }}" alt="" class="w-1/4 mt-5 mb-8">
+                <div class="bg-[#c4dcae] sm:bg-blue-400 md:bg-red-400 lg:bg-white xl:bg-black flex flex-col justify-center items-center"
+                    onmouseenter="enterSaintek()" onmouseleave="leaveSaintek()">
+                    <img src="{{ url('/img/jenis/saintek.png') }}" alt=""
+                        class=" w-5/12 md:w-1/3 lg:w-1/4 mt-5 mb-6 lg:mb-8">
                     <div class="relative w-full">
-                        <div id="one-one" class="absolute w-[10%] drop-shadow-2xl left-[47vw] one-one card">
+                        <div id="one-one"
+                            class="absolute lombacard drop-shadow-2xl left-[40vw] sm:left-[41vw] md:left-[42vw] lg:left-[43vw] one-one card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt=""></div>
@@ -77,7 +75,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div id="one-two" class="absolute w-[10%] drop-shadow-2xl left-[45vw] one-two card">
+                        <div id="one-two"
+                            class="absolute lombacard drop-shadow-2xl left-[41vw] sm:left-[42vw] md:left-[43vw] lg:left-[44vw] one-two card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt="">
@@ -92,7 +91,22 @@
                             </div>
                         </div>
                         <div id="one-three"
-                            class="absolute w-[10%] drop-shadow-2xl left-[43vw] z-50 -rotate-12 one-three card">
+                            class="absolute lombacard drop-shadow-2xl left-[42vw] sm:left-[43vw] md:left-[44vw] lg:left-[45vw] z-50 -rotate-12 one-three card">
+                            <div class="content">
+                                <div class="front">
+                                    <img src="{{ url('/img/lomba/soccer.png') }}" class="w-full" alt="">
+                                </div>
+                                <div
+                                    class="back bg-[#f9db7b] rounded-lg border-[#826541] flex flex-col gap-[1.5vw] items-center justify-center border-[0.3vw] w-full h-full">
+                                    <img src="{{ url('/img/jenis/sacBorder.svg') }}" class="w-[90%] -z-30 absolute"
+                                        alt="">
+                                    <a href="#" class="sacbtn">Daftar</a>
+                                    <a href="#" class="sacbtn">Juknis</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="one-four"
+                            class="absolute lombacard drop-shadow-2xl left-[43vw] sm:left-[44vw] md:left-[45vw] lg:left-[46vw] z-50 -rotate-12 one-four card">
                             <div class="content">
                                 <div class="front">
                                     <img src="{{ url('/img/lomba/soccer.png') }}" class="w-full" alt="">
@@ -107,13 +121,13 @@
                             </div>
                         </div>
                     </div>
-                    <div class=" pt-[20vw]">-</div>
+                    <div class=" pt-[25vw] sm:pt-[23vw] md:pt-[20vw]">-</div>
                 </div>
                 <div class="bg-[#f8e8a0] flex flex-col justify-center items-center" onmouseenter="enterSac()"
                     onmouseleave="leaveSac()">
                     <img src="{{ url('/img/jenis/sac.svg') }}" alt="" class="w-1/4 mt-5 mb-8">
                     <div class="relative w-full">
-                        <div id="two-one" class="absolute w-[10%] drop-shadow-2xl left-[47vw] two-one card">
+                        <div id="two-one" class="absolute lombacard drop-shadow-2xl left-[47vw] two-one card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt=""></div>
@@ -126,7 +140,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div id="two-two" class="absolute w-[10%] drop-shadow-2xl left-[45vw] two-two card">
+                        <div id="two-two" class="absolute lombacard drop-shadow-2xl left-[45vw] two-two card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt="">
@@ -141,7 +155,7 @@
                             </div>
                         </div>
                         <div id="two-three"
-                            class="absolute w-[10%] drop-shadow-2xl left-[43vw] z-50 -rotate-12 two-three card">
+                            class="absolute lombacard drop-shadow-2xl left-[43vw] z-50 -rotate-12 two-three card">
                             <div class="content">
                                 <div class="front">
                                     <img src="{{ url('/img/lomba/soccer.png') }}" class="w-full" alt="">
@@ -162,7 +176,7 @@
                     onmouseleave="leaveLang()">
                     <img src="{{ url('/img/jenis/lang.svg') }}" alt="" class="w-1/4 mt-5 mb-8">
                     <div class="relative w-full">
-                        <div id="three-one" class="absolute w-[10%] drop-shadow-2xl left-[47vw] three-one card">
+                        <div id="three-one" class="absolute lombacard drop-shadow-2xl left-[47vw] three-one card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt=""></div>
@@ -175,7 +189,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div id="three-two" class="absolute w-[10%] drop-shadow-2xl left-[45vw] three-two card">
+                        <div id="three-two" class="absolute lombacard drop-shadow-2xl left-[45vw] three-two card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt="">
@@ -190,7 +204,7 @@
                             </div>
                         </div>
                         <div id="three-three"
-                            class="absolute w-[10%] drop-shadow-2xl left-[43vw] z-50 -rotate-12 three-three card">
+                            class="absolute lombacard drop-shadow-2xl left-[43vw] z-50 -rotate-12 three-three card">
                             <div class="content">
                                 <div class="front">
                                     <img src="{{ url('/img/lomba/soccer.png') }}" class="w-full" alt="">
@@ -211,7 +225,7 @@
                     onmouseleave="leaveJournal()">
                     <img src="{{ url('/img/jenis/journal.svg') }}" alt="" class="w-1/4 mt-5 mb-8">
                     <div class="relative w-full">
-                        <div id="four-one" class="absolute w-[10%] drop-shadow-2xl left-[47vw] four-one card">
+                        <div id="four-one" class="absolute lombacard drop-shadow-2xl left-[47vw] four-one card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt=""></div>
@@ -224,7 +238,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div id="four-two" class="absolute w-[10%] drop-shadow-2xl left-[45vw] four-two card">
+                        <div id="four-two" class="absolute lombacard drop-shadow-2xl left-[45vw] four-two card">
                             <div class="content">
                                 <div class="front"><img src="{{ url('/img/lomba/soccer.png') }}" class="w-full"
                                         alt="">
@@ -239,7 +253,7 @@
                             </div>
                         </div>
                         <div id="four-three"
-                            class="absolute w-[10%] drop-shadow-2xl left-[43vw] z-50 -rotate-12 four-three card">
+                            class="absolute lombacard drop-shadow-2xl left-[43vw] z-50 -rotate-12 four-three card">
                             <div class="content">
                                 <div class="front">
                                     <img src="{{ url('/img/lomba/soccer.png') }}" class="w-full" alt="">
@@ -270,7 +284,7 @@
                 <iframe class=" w-[50vw] h-[28vw] p-[2vw] z-30"
                     src="{{ url('https://www.youtube.com/embed/AEilEiOwFW0') }}"></iframe>
                 <img src="{{ url('img/about.svg') }}" alt="" class="z-30 pt-[3vw] pb-[1vw]">
-                <div class="font-montserrat text-lg z-30 text-center p-[1vw]">
+                <div class="font-montserrat text-[1.4vw] lg:text-base xl:text-lg z-30 text-center p-[1vw]">
                     SONIC LINGUISTIC (Show Your Brain on Science and Link Your Imagination on Language, Sport, Art,
                     Culture, and Journalistic) dahulunya merupakan acara terpisah yaitu Sonic dan Linguistic, keduanya
                     merupakan acara yang diselenggarakan mulai tahun 2002 dan digabung menjadi SONIC LINGUISTIC pada
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,13 @@ Route::get('/home', function () {
     return view('home');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
 Route::get('/upload/{nama}', [UploadController::class, 'upload'])->name('upload');
 Route::post('/uploads/{nama}', [UploadController::class, 'uploadPost'])->name('upload.post');
