5db0c0a83840569039de460a7827c01877a5b944
Rempaah <rmuhammadreff@gmail.com>
2024-01-11 21:01:07 +0700

    Original upload mechanism

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
@@ -14,7 +13,13 @@ class UploadController extends Controller
 
     public function uploadPost(Request $request, $nama)
     {
-        Storage::putFileAs('syarat', $request->file('file'), $request->file->getClientOriginalName());
-        echo Storage::delete('syarat/' . $request->file->getClientOriginalName());
+        $file = $request->file('file');
+        $destinationPath = 'file/syarat/' . $nama;
+
+        if ($file->move($destinationPath, $file->getClientOriginalName())) {
+            echo "succes";
+        } else {
+            echo "failed";
+        }
     }
 }
